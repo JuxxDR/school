f10c8715318aa68119c99f5b3d602dab15876d93
Miguel Colin <maiktmp@gmail.com>
2018-10-20 00:41:04 -0500

    :alien:  Creacióm del template main

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+<div class="footer">
+    <div class="container-fluid">
+        <div class="row" style="height: 50px">
+            <div class="col text-center font-size-0_9 mt-3">
+                <span class="mt-5">Copyright &copy; {{ date('Y') }}. Los aronsitos</span>
+            </div>
+        </div>
+    </div>
+</div>
